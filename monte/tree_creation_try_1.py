fab974da85b213f89d920d19a58e6db992e36fd8
EBYEMJC1 <ebyemjc1@USERWC-NISGUUTP>
2023-11-24 09:03:28 -0800

    mostly done by chatgpt, I legit can't code, unsure if it works, needs to be tested

--- monte/tree_creation_try_1.py
+++ monte/tree_creation_try_1.py
@@ -1,7 +1,10 @@
 import random
+import math
 
 class MonteCarloTreeNode:
-    def __init__(self, state, current_player, parent=None, parent_action=None):
+    # Class variable to store all nodes
+    all_nodes = {}
+    def __init__(self, state, current_player, parent=None):
         # The state of the game represented by the node
         self.state = state
 
@@ -11,9 +14,6 @@ class MonteCarloTreeNode:
         self.opponent = 3 - current_player  # Assuming player values are 1 and 2
         # Reference to the parent node (None for the root node)
         self.parent = parent
-
-        # Action that led to this node from its parent (None for the root node)
-        self.parent_action = parent_action
 
         # List to store child nodes
         self.children = []
@@ -26,6 +26,12 @@ class MonteCarloTreeNode:
         self.wins = 0
         self.losses = 0
         self.game_over = False
+        
+        # Add the current node to the dictionary hold refrence to self so if self changes so does the value of the dictionary at state
+        MonteCarloTreeNode.all_nodes[self.state] = self
+
+    def find_node_if_in_tree(initial_state):
+        return MonteCarloTreeNode.all_nodes.get(initial_state, None)
 
     def get_legal_actions(self):
         legal_actions = []
@@ -34,7 +40,7 @@ class MonteCarloTreeNode:
                 new_state = [row.copy() for row in self.state]
                 for row in reversed(range(len(new_state))):
                     if new_state[row][column] == 0:
-                        new_state[row][column] = self.current_player
+                        new_state[row][column] = self.opponent #could be self.current state I'm unsure
                         legal_actions.append(new_state)
                         break  # Break once you've placed a piece in the column
         return legal_actions
@@ -76,14 +82,39 @@ class MonteCarloTreeNode:
                     return True
         return False
     
+    def selection(self):
+        selected_child = None
+        if not self.is_fully_expanded():
+            selected_child = self.expand_current_node()
+            return selected_child
+        else:
+            #UCB = wins/visits + exploration_factor * sqrt(log(total_visits)/visits)
+            exploration_factor = math.sqrt(2)
+            choosen_ucb_is_max_ucb = float('-inf')
+            for child in self.children:
+                exploitation_factor=(child.wins/child.visits)
+                ucb=exploitation_factor+exploitation_factor*math.sqrt(math.log(self.visits)/child.visits)
+                if ucb > choosen_ucb_is_max_ucb:
+                    choosen_ucb_is_max_ucb = ucb
+                    selected_child = child
+            return selected_child
+
+    def expand_current_node(self):
+        action = self.untried_actions.pop(0)
+        child_node = MonteCarloTreeNode(action,self.opponent, self)
+        self.children.append(child_node)
+        return child_node
+    
+    def is_fully_expanded(self):
+        return len(self.untried_actions)==0
 
     def random_choices_when_utc_unknown_called_rollout_simulation(self):
-        current_random_state = self.state
+        current_random_state = self.state.copy()
         current_player = self.current_player
         while not current_random_state.is_terminal_and_win() or current_random_state.get_legal_actions():#while not a win or lose and there are still more possible actions
             legal_actions = current_random_state.get_legal_actions()
-            random_action = MonteCarloTreeNode(random.choice(legal_actions), 3-self.current_player, current_random_state)
-            current_random_state = random_action.state
+            random_action = MonteCarloTreeNode(random.choice(legal_actions), self.opponent, current_random_state)
+            current_random_state = random_action.state.copy()
 
             if not legal_actions:
                 return 0
@@ -92,3 +123,33 @@ class MonteCarloTreeNode:
                     return 1
                 else:
                     return -1
+    
+    def backpropogate_assign_wins_and_losses_after_simulation(self, result):
+        current_node = self
+        while current_node is not None:
+            current_node.visits += 1
+            if result == 1:
+                current_node.wins += 1
+            elif result == -1:
+                current_node.losses += 1
+            else:
+                pass
+            current_node = current_node.parent
+def monte_carlo_tree_search(initial_state, iterations=1000, current_player=1):
+    # Check if the tree already has a node with the initial state
+    root_node = MonteCarloTreeNode.find_node_if_in_tree(initial_state)
+    # If not, create a new root node with the initial state
+    if root_node is None:
+        root_node = MonteCarloTreeNode(initial_state, current_player=current_player)
+    else:
+        root_node = MonteCarloTreeNode.all_nodes[initial_state]
+    for _ in range(iterations):
+        selected_node = root_node.selection()
+        if not selected_node.is_terminal_and_win():
+            child_node = selected_node.expand_current_node()
+            result = child_node.random_choices_when_utc_unknown_called_rollout_simulation()
+            child_node.backpropogate_assign_wins_and_losses_after_simulation(result)
+    # After the search is complete, choose the best move based on the statistics
+    best_child = max(root_node.children, key=lambda x: x.visits)
+    best_move = best_child.state
+    return best_move
