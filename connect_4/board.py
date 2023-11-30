e044a9f4d2961d5a11c5c79a99fbda9e25d71958
PMarcoG10 <marcog10@csuf.fullerton.edu>
2023-11-30 13:16:15 -0800

    gotta fix borders

--- connect_4/board.py
+++ connect_4/board.py
@@ -3,13 +3,13 @@
 import pygame
 import sys
 import numpy as np
+import connect_4.rgbcolors
 
 
 class Board:
     """Sets up the board for the game"""
 
     def __init__(self):
-
         pygame.init()
 
         self.ROW_COUNT = 6
@@ -17,23 +17,33 @@ class Board:
         self.board = np.zeros((self.ROW_COUNT, self.COLUMN_COUNT))
 
     def create_board(self):
+        """Creating the board layout"""
         return np.zeros((self.ROW_COUNT, self.COLUMN_COUNT))
 
     def drop_piece(self, row, col, piece):
+        """Handles when a piece is dropped"""
         self.board[row][col] = piece
 
     def valid_location(self, col):
-        return self.board[self.ROW_COUNT - 1][col] == 0 if 0 <= col < self.COLUMN_COUNT else False
+        """Checks for a valid location if piece is dropped"""
+        return (
+            self.board[self.ROW_COUNT - 1][col] == 0
+            if 0 <= col < self.COLUMN_COUNT
+            else False
+        )
 
     def open_row(self, col):
+        """Checks for the next open row"""
         for r in range(self.ROW_COUNT):
             if self.board[r][col] == 0:
                 return r
 
     def print_board(self):
+        """Prints the board on the terminal"""
         print(np.flip(self.board, 0))
 
     def winning_move(self, piece):
+        """Checks for the winning piece"""
         for c in range(self.COLUMN_COUNT - 3):
             for r in range(self.ROW_COUNT):
                 if (
@@ -77,11 +87,13 @@ class Board:
         return False
 
     def draw_board(self, screen, SQUARESIZE, RADIUS):
+        """Draws the board on screen"""
+        # board drawing
         for c in range(self.COLUMN_COUNT):
             for r in range(self.ROW_COUNT):
                 pygame.draw.rect(
                     screen,
-                    (0, 0, 255),
+                    connect_4.rgbcolors.blue,
                     (
                         c * SQUARESIZE,
                         r * SQUARESIZE + SQUARESIZE,
@@ -91,7 +103,7 @@ class Board:
                 )
                 pygame.draw.circle(
                     screen,
-                    (0, 0, 0),
+                    connect_4.rgbcolors.light_blue,
                     (
                         int(c * SQUARESIZE + SQUARESIZE / 2),
                         int(r * SQUARESIZE + SQUARESIZE + SQUARESIZE / 2),
@@ -99,12 +111,13 @@ class Board:
                     RADIUS,
                 )
 
+        # player pieces
         for c in range(self.COLUMN_COUNT):
             for r in range(self.ROW_COUNT):
                 if self.board[r][c] == 1:
                     pygame.draw.circle(
                         screen,
-                        (255, 0, 0),
+                        connect_4.rgbcolors.red,
                         (
                             int(c * SQUARESIZE + SQUARESIZE / 2),
                             screen.get_height() - int(r * SQUARESIZE + SQUARESIZE / 2),
@@ -114,7 +127,7 @@ class Board:
                 elif self.board[r][c] == 2:
                     pygame.draw.circle(
                         screen,
-                        (255, 255, 0),
+                        connect_4.rgbcolors.yellow,
                         (
                             int(c * SQUARESIZE + SQUARESIZE / 2),
                             screen.get_height() - int(r * SQUARESIZE + SQUARESIZE / 2),
--- connect_4/player.py
+++ connect_4/player.py
@@ -4,6 +4,7 @@ import pygame
 import sys
 import math
 from connect_4.board import Board
+import connect_4.rgbcolors
 
 
 class PlayerGame:
@@ -11,6 +12,7 @@ class PlayerGame:
 
     def __init__(self, screen):
         pygame.init()
+        
         self.board = Board()
         self.game_over = False
         self.turn = 0
@@ -25,49 +27,18 @@ class PlayerGame:
         self.screen = screen
         self.font = pygame.font.Font(None, 36)
 
-
     def draw_board(self):
-        self.screen.fill((40, 40, 40))  # Fill background color
-
-        for c in range(self.board.COLUMN_COUNT):
-            for r in range(self.board.ROW_COUNT):
-                pygame.draw.rect(
-                    self.screen,
-                    (0, 0, 255),
-                    (c * self.SQUARESIZE, r * self.SQUARESIZE + self.SQUARESIZE, self.SQUARESIZE, self.SQUARESIZE),
-                )
-                pygame.draw.circle(
-                    self.screen,
-                    (173, 216, 230),
-                    (int(c * self.SQUARESIZE + self.SQUARESIZE / 2), int(r * self.SQUARESIZE + self.SQUARESIZE + self.SQUARESIZE / 2)),
-                    self.RADIUS,
-                )
-
-        for c in range(self.board.COLUMN_COUNT):
-            for r in range(self.board.ROW_COUNT):
-                if self.board.board[r][c] == 1:
-                    pygame.draw.circle(
-                        self.screen,
-                        (255, 0, 0),
-                        (int(c * self.SQUARESIZE + self.SQUARESIZE / 2), self.screen.get_height() - int(r * self.SQUARESIZE + self.SQUARESIZE / 2)),
-                        self.RADIUS,
-                    )
-                elif self.board.board[r][c] == 2:
-                    pygame.draw.circle(
-                        self.screen,
-                        (255, 255, 0),
-                        (int(c * self.SQUARESIZE + self.SQUARESIZE / 2), self.screen.get_height() - int(r * self.SQUARESIZE + self.SQUARESIZE / 2)),
-                        self.RADIUS,
-                    )
-
-        pygame.display.update()
+        """Calls the drawboard function from Board Class"""
+        self.board.draw_board(self.screen, self.SQUARESIZE, self.RADIUS)
 
     def reset_game(self):
+        """When the game is over restart"""
         self.board = Board()
         self.game_over = False
         self.turn = 0
 
     def run(self):
+        """Runs the game"""
         while not self.game_over:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -75,20 +46,22 @@ class PlayerGame:
 
                 if event.type == pygame.MOUSEMOTION:
                     # Draw the background only once
-                    pygame.draw.rect(self.screen, (173, 216, 230), (0, 0, self.width, self.SQUARESIZE))
+                    pygame.draw.rect(
+                        self.screen, LIGHTBLUE, (0, 0, self.width, self.SQUARESIZE)
+                    )
 
                     posx = event.pos[0]
                     if self.turn == 0:
                         pygame.draw.circle(
                             self.screen,
-                            (255, 0, 0),
+                            connect_4.rgbcolors.red,
                             (posx, int(self.SQUARESIZE / 2)),
                             self.RADIUS,
                         )
                     else:
                         pygame.draw.circle(
                             self.screen,
-                            (255, 255, 0),
+                            connect_4.rgbcolors.yellow,
                             (posx, int(self.SQUARESIZE / 2)),
                             self.RADIUS,
                         )
@@ -97,7 +70,9 @@ class PlayerGame:
 
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     pygame.draw.rect(
-                        self.screen, (173, 216, 230), (0, 0, self.width, self.SQUARESIZE)
+                        self.screen,
+                        connect_4.rgbcolors.light_blue,
+                        (0, 0, self.width, self.SQUARESIZE),
                     )
                     posx = event.pos[0]
                     col = int(math.floor(posx / self.SQUARESIZE))
@@ -117,13 +92,5 @@ class PlayerGame:
 
                 # Draw the board and update the display continuously
                 self.draw_board()
-
-                # Show winning message for 1 second
-                current_time = pygame.time.get_ticks()
-                if self.game_over and current_time - self.last_win_time < 1000:
-                    winner = "Player 1" if self.turn == 0 else "Player 2"
-                    print(f"{winner} wins!")  # Modify or remove this line as needed
-                    pygame.time.wait(1000)  # Wait for 1 second
-                    self.reset_game()
 
                 pygame.display.update()
